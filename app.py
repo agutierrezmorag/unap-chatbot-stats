47ff8225260b1a58e947bb6dcde183376ddc3ac4
agutierrezmorag <alvarogutierrezmoraga@gmail.com>
2024-01-13 19:13:38 -0300

    Refactor chat filtering and date selection in main function

--- app.py
+++ app.py
@@ -31,17 +31,59 @@ def get_chat_count():
 def main():
     st.set_page_config(page_title="Chatbot data", page_icon="📈", layout="wide")
     st.title("📊 UNAP Chatbot Data")
+
     messages = load_messages()
     df = pd.json_normalize(messages, sep="_")
+    df["submission_time"] = pd.to_datetime(df["submission_time"])
 
-    chat_types = df["chat_type"].unique().tolist()
-    chat_types.insert(0, "Todo")
-    selected_chat_type = st.selectbox("Tipo de chat", options=chat_types)
+    col1, col2, col3 = st.columns(3)
 
-    if selected_chat_type == "Todo":
+    with col1:
+        chat_types = df["chat_type"].unique().tolist()
+        chat_types.insert(0, "--")
+        selected_chat_type = st.selectbox("Tipo de chat", options=chat_types)
+
+    with col2:
+        user_scores = df["user_feedback_score"].unique().tolist()
+        user_scores.insert(0, "--")
+        selected_user_score = st.selectbox("Puntaje de usuario", options=user_scores)
+
+    with col3:
+        scol1, scol2 = st.columns(2)
+        with scol1:
+            start_date = st.date_input(
+                "Fecha de inicio",
+                df["submission_time"].min().date(),
+                min_value=df["submission_time"].min().date(),
+                max_value=df["submission_time"].max().date(),
+            )
+        with scol2:
+            end_date = st.date_input(
+                "Fecha de término",
+                df["submission_time"].max().date(),
+                min_value=df["submission_time"].min().date(),
+                max_value=df["submission_time"].max().date(),
+            )
+
+    if selected_chat_type == "--":
         filtered_messages = df
     else:
         filtered_messages = df[df["chat_type"] == selected_chat_type]
+
+    if selected_user_score == "--":
+        filtered_messages = filtered_messages
+    else:
+        filtered_messages = filtered_messages[
+            filtered_messages["user_feedback_score"] == selected_user_score
+        ]
+
+    if start_date > end_date:
+        st.error("Error: End date must fall after start date.")
+    else:
+        filtered_messages = df[
+            (df["submission_time"].dt.date >= start_date)
+            & (df["submission_time"].dt.date <= end_date)
+        ]
 
     avg_time_to_answer_all = df["time_to_answer"].mean()
     avg_time_to_answer_selected = filtered_messages["time_to_answer"].mean()
@@ -55,6 +97,8 @@ def main():
     total_cost_all = df["tokens_total_cost_usd"].sum()
     total_cost_selected = filtered_messages["tokens_total_cost_usd"].sum()
     delta_cost = total_cost_selected - total_cost_all
+
+    st.write(filtered_messages)
 
     col1, col2, col3, col4 = st.columns(4)
     with col1:
@@ -78,8 +122,6 @@ def main():
             total_cost_selected,
             delta=delta_cost,
         )
-
-    st.write(filtered_messages)
 
 
 if __name__ == "__main__":
